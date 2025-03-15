4f09bc9b3b3226d2bcb1b513b83996bd4894a7f8
Asad Mehmood <143835996+theasad19@users.noreply.github.com>
2025-03-16 03:41:37 +0500

    Add files via upload

---
+++ web.js
@@ -0,0 +1,42 @@
+var typed = new Typed('.text', {
+    strings: ["Web Developer", "Android Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+var typed = new Typed('.texte', {
+    strings: ["MERN Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+var typed = new Typed('.texti', {
+    strings: ["Voice Assistant Using Python"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
+    loop: true
+});
+var typed = new Typed('.textii', {
+    strings: ["Intruder Alert System"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
+    loop: true
+});
+var typed = new Typed('.textiii', {
+    strings: ["Video Player App"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
+    loop: true
+});
+var typed = new Typed('.textiiii', {
+    strings: ["Grocery Store Managements System"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
+    loop: true
+});
